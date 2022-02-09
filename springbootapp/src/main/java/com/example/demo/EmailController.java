5363c5a89e799386659f7feaad2e3008f4c2c7e1
Your Name <you@example.com>
2022-02-09 10:10:33 +0100

    reseni proxy

--- springbootapp/src/main/java/com/example/demo/EmailController.java
+++ springbootapp/src/main/java/com/example/demo/EmailController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmailController {
 
-    @Async
+    @Autowired
+    private EmailService emailService; // tady je proxy
+
+    // tohle moc nepouzivat:
+//    @Lazy
+//    @Autowired
+//    private EmailController self;
+
+//    @Async
     @GetMapping("/sendEmails") // GET operace by mely byt "idempotent" tzn. ze by nemely mit zadne "side-effects"
     public void sendEmails() {
         for (int i = 0; i < 100; i++) {
-            sendEmail(i);
+//            sendEmail(i);
+            // MUSI SE PRISTUPOVAT PRES PROXY!!!
+            emailService.sendEmail(i);
+//            self.sendEmail(i);
         }
     }
 
-    @SneakyThrows // tohle nepouzivat :-) jenom na skoleni :-))
-    public void sendEmail(int i) {
-        log.info("sending email {}", i);
-        Thread.sleep(100);
-    }
+//    @Async
+//    @SneakyThrows // tohle nepouzivat :-) jenom na skoleni :-))
+//    public void sendEmail(int i) {
+//        log.info("sending email {}", i);
+//        Thread.sleep(100);
+//    }
 
 }
